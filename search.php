c8b9df3395ea7e6441f015e143f2afca4af8f7e9
Jason Giambona <jason.giambona@gmail.com>
2020-05-21 16:38:21 +0800

    Initial commit of unchanged template

---
+++ search.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>search result</title>
+<meta charset="utf-8">    
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+<meta name="description" content="Your description">
+<meta name="keywords" content="Your keywords">
+<meta name="author" content="Your name">
+<!--CSS-->
+<link rel="stylesheet" href="css/bootstrap.css" >
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="fonts/font-awesome.css">
+<!--JS-->
+<script src="js/jquery.js"></script>
+<script src="js/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="search/search.js"></script>
+<script src="js/jquery.mobilemenu.js"></script> 
+<script src="js/jquery.ui.totop.js"></script>
+<script src="js/jquery.equalheights.js"></script> 
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+
+    <div id="ie6-alert" style="width: 100%; text-align:center;">
+    <img src="http://beatie6.frontcube.com/images/ie6.jpg" alt="Upgrade IE 6" width="640" height="344" border="0" usemap="#Map" longdesc="http://die6.frontcube.com" />
+      <map name="Map" id="Map"><area shape="rect" coords="496,201,604,329" href="http://www.microsoft.com/windows/internet-explorer/default.aspx" target="_blank" alt="Download Interent Explorer" /><area shape="rect" coords="380,201,488,329" href="http://www.apple.com/safari/download/" target="_blank" alt="Download Apple Safari" /><area shape="rect" coords="268,202,376,330" href="http://www.opera.com/download/" target="_blank" alt="Download Opera" /><area shape="rect" coords="155,202,263,330" href="http://www.mozilla.com/" target="_blank" alt="Download Firefox" />
+        <area shape="rect" coords="35,201,143,329" href="http://www.google.com/chrome" target="_blank" alt="Download Google Chrome" />
+      </map>
+  </div>
+  <![endif]-->
+</head>
+<body>
+<!--header-->
+<header id="stuck_container">  
+    <div class="container"> 
+        <h1 class="navbar-brand navbar-brand_"><a href="index.html"><img src="img/logo.png" alt="logo"></a></h1>
+        <nav class="navbar navbar-default navbar-static-top tm_navbar clearfix" role="navigation">
+            <ul class="nav sf-menu clearfix">
+                <li><a href="index.html">main</a></li>
+                <li class="sub-menu"><a href="index-1.html">about us</a><span></span>
+                    <ul class="submenu">
+        				<li><a href="#">history</a><span></span>
+                            <ul class="submenu">
+                                <li><a href="#">fresh</a></li>
+                                <li><a href="#">archive</a></li>
+                            </ul> 
+                        </li>
+        				<li><a href="#">advice</a></li>
+                        <li><a href="#">news</a></li>
+        			</ul>
+                </li>
+                <li><a href="index-2.html">for sale</a></li>
+                <li><a href="index-3.html">blog</a></li>
+                <li><a href="index-4.html">Contacts</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<div class="global indent">
+    <!--content-->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <div class="box">
+                    <h2>Search result:</h2>
+                    <div id="search-results"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--footer-->
+<footer>
+    <i></i>
+    <div class="container">
+        <div class="col-lg-5 col-md-5 col-sm-5">
+            <figure><img src="img/foo_logo.png" alt=""></figure>
+            <p>&copy; <em id="copyright-year"></em> <a href="index-5.html">Privacy policy</a></p>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4">
+            <ul class="follow_icon">
+                <li><a href="#"><img src="img/follow_icon1.png" alt=""></a></li>
+                <li><a href="#"><img src="img/follow_icon2.png" alt=""></a></li>
+                <li><a href="#"><img src="img/follow_icon3.png" alt=""></a></li>
+                <li><a href="#"><img src="img/follow_icon4.png" alt=""></a></li>
+            </ul>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-3">
+            <p>8901 Marmora Road Glasgow, <br>DO4 89GR</p>
+        </div>
+    </div>
+</footer>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script>
+</body>
+</html>
